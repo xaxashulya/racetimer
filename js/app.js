32d4976facb381c70de99bb668662fe8d1b1437e
xaxashulya <xaxa.shulya@gmail.com>
2026-08-04 10:33:14 +0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -9,7 +9,7 @@
   // =================================================================
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
+const firebase_Config = {
   apiKey: "AIzaSyBncAe5D5L41KZCBSfHb707t402g45s6m4",
   authDomain: "app-racetimer.firebaseapp.com",
   databaseURL: "https://app-racetimer-default-rtdb.firebaseio.com",
